97002d9982cb630e509e70c79b4f3da319291d06
Małgorzata Gałężewska <mgalezewska@opi.org.pl>
2015-01-23 11:30:25 +0100

    odwrócona lista tłumaczeń

--- src/main/java/pl/org/mgalezewska/memo/bean/MemoBean.java
+++ src/main/java/pl/org/mgalezewska/memo/bean/MemoBean.java
@@ -1,5 +1,6 @@
 package pl.org.mgalezewska.memo.bean;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.csv.CSVRecord;
 import org.primefaces.model.UploadedFile;
 import pl.org.mgalezewska.memo.bo.MemoBO;
@@ -11,6 +12,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -25,21 +27,29 @@ public class MemoBean {
 
     private FileManager fileManager = new FileManager();
 
+    private List<MemoBO> memos = Lists.newArrayList();
+
     public void upload() throws IOException {
         if(file != null) {
             FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
 
             fileManager.saveFile(file);
-
+            readRecords();
         }
     }
 
-    public List<MemoBO> readRecords() {
-        CSVReader reader = new CSVReader();
-        return reader.read(fileManager.getPath());
+    public List<MemoBO> getTransations() {
+        List<MemoBO> reverses = Lists.newArrayList();
+        reverses.addAll(memos);
+        Collections.reverse(reverses);
+        return reverses;
     }
 
+    private void readRecords() {
+        CSVReader reader = new CSVReader();
+        memos = reader.read(fileManager.getPath());
+    }
 
     public UploadedFile getFile() {
         return file;
@@ -47,5 +57,13 @@ public class MemoBean {
 
     public void setFile(UploadedFile file) {
         this.file = file;
+    }
+
+    public List<MemoBO> getMemos() {
+        return memos;
+    }
+
+    public void setMemos(List<MemoBO> memos) {
+        this.memos = memos;
     }
 }
