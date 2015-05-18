be4b1ca476f6fd9cbe356c2d4dce046cc48fd44a
dgrebowiec <dgrebowiec@nexio.pl>
2015-05-18 21:01:20 +0200

    tabele board i memo

--- mp-persistence/src/main/java/pl/org/mgalezewska/memo/persistence/dao/common/GenericRepository.java
+++ mp-persistence/src/main/java/pl/org/mgalezewska/memo/persistence/dao/common/GenericRepository.java
@@ -1,17 +1,3 @@
-/*
- * To oprogramowanie jest w³asnoœci¹
- *
- * OPI - Oœrodek Przetwarzania Informacji,
- * Al. Niepodleg³oœci 188B, 00-608 Warszawa
- * Numer KRS: 0000127372
- * S¹d Rejonowy dla m. st. Warszawy w Warszawie XII Wydzia³
- * Gospodarczy KRS
- * REGON: 006746090
- * NIP: 525-000-91-40
- * Wszystkie prawa zastrze¿one. To oprogramowanie mo¿e byæ u¿ywane tylko
- * zgodnie z przeznaczeniem. OPI nie odpowiada za ewentualne wadliwe
- * dzia³anie kodu.
- */
 package pl.org.mgalezewska.memo.persistence.dao.common;
 
 import com.mysema.query.types.EntityPath;
@@ -24,7 +10,7 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 /**
- * @author Ma³gorzata Ga³ê¿ewska <mgalezewska@opi.org.pl>
+ * @author Malgorzata Galezewska <mgalezewska@opi.org.pl>
  */
 public abstract class GenericRepository<T, ID extends Serializable> {
 
